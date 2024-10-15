9d3601d0417d3f2da6e0781cf1e7b7538d351612
DUBSVANANOGIT <arthur.bic_@hotmail.com>
2024-10-15 10:29:48 -0300

    node pg, express e fetch

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const app = express();
+const { Client } = require('pg');
+const express = require('express')
+const connectionString = 'postgresql://postgres.pflqrsmtkbifbqicvqus:rTOUJqXRbbxW6Bib@aws-0-sa-east-1.pooler.supabase.com:6543/postgres' ;
+const app = express()
+const client = new Client({
+    connectionString,
+});
+client.connect();
 
-app.get('/tarefas', (req, res) => {
-    res.send("ola ola ola")
-})
-
-app.post('/tarefas', (req, res) => {
+app.get('/tarefas',async (req,res)=> {
+    try {
+        const result = await client.query('SELECT * FROM tabelas');
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro buscando tarefa")
+    }
 });
 
-app.listen(3000, function() {
-    console.log("Server rondando...")
-})
+app.listen(3000, () => console.log('Server rodando porta 3000'));
+
+(async () => {
+    await client.connect();
+    const res = await client.query('SELECT * FROM tabelas)');
+    console.log(res.rows);
+    await client.end();
+})().catch(err => console.error("Erro ao conectar ao banco de dados:", err))
